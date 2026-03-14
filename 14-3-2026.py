38434788d5ad63398acabff8cf92a92fd90555c7
Youssef Belal <228213163+ysfbelal-code@users.noreply.github.com>
2026-03-14 11:10:28 +0300

    Update 14-3-2026.py

--- 14-3-2026.py
+++ 14-3-2026.py
@@ -4,9 +4,9 @@ import time
 
 st.session_state.setdefault("conversation", [])
 
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 local_css("style.css")
 
