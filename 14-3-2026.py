def84b35ff0081321893e769b07af3a4811fa4a3
Youssef Belal <228213163+ysfbelal-code@users.noreply.github.com>
2026-03-14 11:20:56 +0300

    Update 14-3-2026.py

--- 14-3-2026.py
+++ 14-3-2026.py
@@ -9,7 +9,7 @@ st.markdown("""
 @import url('');
 
 html, body, [class*="css"], [class*="st-"], .stApp, .stApp * {
-    font-family: 'Source Sans Pro', Source, sans-serif !important;
+    font-family: 'Yu Gothic UI Light', Yu, sans-serif !important;
 }
 </style>
 """, unsafe_allow_html=True)
