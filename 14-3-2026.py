72c3652590ad0d9b842a6bd12ff0662b92108815
Youssef Belal <228213163+ysfbelal-code@users.noreply.github.com>
2026-03-14 10:57:29 +0300

    Update 14-3-2026.py

--- 14-3-2026.py
+++ 14-3-2026.py
@@ -1,9 +1,18 @@
 import streamlit as st
-from groq import generate_response
+from groq_api import generate_response
 import time
 
+st.markdown("""
+    <style>
+            html, body [class*='css'] {
+            font-family: 'Times New Roman', monospace;
+            font-size: 18px;
+            }
+    </style>""", unsafe_allow_html=True
+)
+
 st.session_state.setdefault("conversation", [])
-st.title("ENHANCED AI TEACHING ASSISTANT")
+st.title("ENHANCED AI TEACHING ASSISTANT", text_alignment='center')
 st.text_input("How can I help you today?")
 
 c1, c2, c3 = st.columns([1, 1, 1])
