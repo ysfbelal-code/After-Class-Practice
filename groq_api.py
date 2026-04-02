5d03df1ee41dec58a4b611487f377de05ac261ad
Youssef Belal <228213163+ysfbelal-code@users.noreply.github.com>
2026-04-02 19:47:09 +0300

    Update groq_api.py

--- groq_api.py
+++ groq_api.py
@@ -1,19 +1,18 @@
 import streamlit as st
 from openai import OpenAI
-import config
 
 @st.cache_data(show_spinner=False)
 
 def generate_response(prompt: str, temperature: float = 0.3, tokens: int = 8192) -> str:
     url = "https://api.groq.com/openai/v1"
-    apikey = config.hf_api_key
-    models = getattr(config, 'GROQ_MODELS', 
+    apikey = st.secrets['hf_api']
+    models = st.secrets.get('GROQ_MODELS', 
                             ['meta-llama/llama-4-scout-17b-16e-instruct', 
                             'llama-3.1-8b-instant', 
                             'llama-3.3-70b-versatile',])
     
     if not apikey:
-        return "Error: hf_api_key missing in secrets"
+        return "Error: hf_api missing in secrets"
 
     last_err = None
     for m in models:
@@ -21,7 +20,7 @@ def generate_response(prompt: str, temperature: float = 0.3, tokens: int = 8192)
             c = OpenAI(api_key=apikey, base_url=url)
             r = c.chat.completions.create(
                 model=m,
-                messages=[{'role':'system', "content": "You follow instructions exactly. Output ONLY what is asked — no preamble, no explanation, no extra text."}, 
+                messages=[{'role':'system', "content": "You follow instructions exactly. Output ONLY what is asked — no preamble, no explanation, no extra text. If there's a word with multiple meanings in the other language, output the first thing that comes to mind. Make sure the translations are gramatically correct. Avoid outputs like \frac{2420}{20} when solving equations. USe the actual mathematical symbols if you can, and make sure they don't overlap the page or don't fully appear."}, 
                 {'role': 'user', 'content': prompt}],
                 temperature=temperature,
                 max_tokens=tokens
